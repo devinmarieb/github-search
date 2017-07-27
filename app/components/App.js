aa266523b1ba4fec5f79f7dc8a5dd4a9d96e41ef
Devin Beliveau <devinmarieb@gmail.com>
2017-07-26 20:12:29 -0600

    Add * to language

--- app/components/App.js
+++ app/components/App.js
@@ -14,7 +14,7 @@ export default class App extends Component {
     super()
     this.state = {
       searchWord: '',
-      languageWord: '',
+      languageWord: '*',
       orderWord: 'desc',
       sortWord: '',
       relevantSort: '',
